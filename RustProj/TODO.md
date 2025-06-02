0c35f7503d2d820ab39376eead00fe486538469b
JedayTb <ethansuper32@gmail.com>
2025-06-02 17:14:18 -0400

    Rust project files added

---
+++ RustProj/TODO.md
@@ -0,0 +1,7 @@
+Most recent TODO update - 2025-05-24 2:41pm
+
+RODIO Supports - MP3, WAV, VORBIS, FLAC , MP4 and AAC (Disabled by default, only handle by symphonia)
+
+- Implement Song play queue 
+- Current playing song 
+- Better UI.
---
+++ RustProj/src/main.rs
@@ -0,0 +1,135 @@
+use fltk::{app, button::Button, frame::Frame, group::Flex, prelude::*, window::Window};
+use fltk_theme::{ColorTheme, color_themes};
+use rodio::{Decoder, OutputStream, Sink};
+use std::cell::RefCell;
+use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::rc::Rc;
+fn main() {
+    let app = app::App::default().with_scheme(app::Scheme::Gtk);
+    let theme = ColorTheme::new(color_themes::BLACK_THEME);
+    theme.apply();
+    let mut wind = Window::new(500, 500, 680, 360, "RMP");
+
+    let mut button_box = Flex::default().with_size(620, 45).row().with_pos(30, 300);
+
+    let mut last_song_button = Button::default().with_label("<");
+    let mut pause_song_button = Button::default().with_label("Pause");
+    let mut next_song_button = Button::default().with_label(">");
+
+    button_box.end();
+
+    let mut queue_list = Flex::default()
+        .column()
+        .with_size(65, 300)
+        .right_of(&button_box, 0);
+    let frame = Frame::default().with_label("Test");
+
+    let play_queue = Rc::new(RefCell::new(scan_directory("TestMusicFiles")));
+    let play_queue_last = Rc::clone(&play_queue);
+    let play_queue_next = Rc::clone(&play_queue);
+
+    let current_song_index = Rc::new(RefCell::new(0usize));
+    let index_next_pointer = Rc::clone(&current_song_index);
+    let index_last_pointer = Rc::clone(&current_song_index);
+    //const TESTMP3PATH: &str = "TestMusicFiles/07 Alright.mp3";
+    // Get an output steam handle to the default physical sound device
+    // Note that no sound will be played if _stream is droppped;
+    // Stream must live as long as sink
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Rc::new(RefCell::new(Sink::try_new(&stream_handle).unwrap()));
+    let sink_pause = Rc::clone(&sink);
+    let sink_next = Rc::clone(&sink);
+    let sink_last = Rc::clone(&sink);
+    // load a sound from a file, using a path relative to cargo.toml
+    let file = BufReader::new(File::open(play_queue.borrow()[0].clone()).unwrap());
+
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Sink has weird behavior where it will play
+    // song after having it be appended
+    sink.borrow().append(source);
+    sink.borrow().pause();
+
+    last_song_button.set_callback(move |_| {
+        let curr_ind = *index_last_pointer.borrow();
+        let new_ind = back_a_song(&sink_last.borrow(), &play_queue_last.borrow(), curr_ind);
+        *index_last_pointer.borrow_mut() = new_ind;
+    });
+
+    next_song_button.set_callback(move |_| {
+        let curr_ind = *index_next_pointer.borrow();
+        let new_ind = next_song_in_queue(&sink_next.borrow(), &play_queue_next.borrow(), curr_ind);
+        *index_next_pointer.borrow_mut() = new_ind;
+    });
+
+    pause_song_button.set_callback(move |btn| {
+        if sink_pause.borrow().is_paused() {
+            sink.borrow().play();
+            btn.set_label("Pause");
+        } else {
+            sink_pause.borrow().pause();
+            btn.set_label("Play");
+        }
+    });
+    wind.end();
+    wind.show();
+
+    app.run().unwrap();
+}
+
+fn list_queue(play_queue: &Vec<String>, current_song_index: usize) {
+    let mut i: i32 = 0;
+    let mut print_string: String = String::from("");
+    for song_name in play_queue {
+        print_string.push_str(song_name);
+        i += 1;
+        if i == current_song_index as i32 {
+            print_string.push_str("\t<--- Listening");
+        }
+        print_string.push_str("\n");
+    }
+    println!("{}", print_string);
+}
+fn back_a_song(sink: &Sink, play_queue: &Vec<String>, current_song_index: usize) -> usize {
+    let mut new_ind: usize = current_song_index;
+    if new_ind != 0 {
+        new_ind -= 1;
+    }
+    let next_song = BufReader::new(File::open(play_queue[new_ind].clone()).unwrap());
+    let source = Decoder::new(next_song).unwrap();
+    // Stops playback and clears all appened files
+    sink.stop();
+    sink.append(source);
+    sink.play();
+
+    return new_ind;
+}
+fn next_song_in_queue(sink: &Sink, play_queue: &Vec<String>, current_song_index: usize) -> usize {
+    let mut new_ind: usize = current_song_index;
+    new_ind += 1;
+
+    if new_ind > play_queue.len() - 1 {
+        new_ind -= 1;
+    }
+    let next_song = BufReader::new(File::open(play_queue[new_ind].clone()).unwrap());
+    let source = Decoder::new(next_song).unwrap();
+    // Stops playback and clears all appened files
+    sink.stop();
+    sink.append(source);
+    sink.play();
+
+    return new_ind;
+}
+fn scan_directory(dir_path: &str) -> Vec<String> {
+    // Scans for files in the given directory.
+    let pathsindir = fs::read_dir(dir_path).unwrap();
+    let mut plqueue: Vec<String> = Vec::new();
+    for path in pathsindir {
+        //println!("Name: {}", path.unwrap().path().display());
+        plqueue.push(path.unwrap().path().display().to_string());
+    }
+    return plqueue;
+}
